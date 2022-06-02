32ae13a17c6489381db8f7f59ba363286b846e66
Sara Rossow <rossowka@icloud.com>
2022-06-02 10:07:08 +0200

    added styles

--- src/BlogTile.js
+++ src/BlogTile.js
@@ -1,7 +1,18 @@
+const styles = {
+    backgroundColor: '#f5f6fa',
+    border: '#353b48 1px solid',
+    borderRadius: 10,
+    padding: 10,
+    margin: 20,
+    header: {
+        color: '#e84118',
+    }
+}
+
 function BlogTile({ title, intro}) {
     return (
-        <div>
-            <h2>{title}</h2>
+        <div style={styles}>
+            <h2 style={styles.header}>{title}</h2>
             <p>{intro}</p>
         </div>
     );
